e183544511c9d9645618d428a24d29644730aba5
Nikol Georgieva <nicole.georgieva18@abv.bg>
2023-11-15 21:45:09 +0200

    Count characters in string solution

--- src/com/company/CountCharactersInString.java
+++ src/com/company/CountCharactersInString.java
@@ -1,9 +1,24 @@
 package com.company;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 //The main idea is to count all the occurring characters in a string.
 //If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
 //What if the string is empty? Then the result should be empty object literal, {}.
 
 public class CountCharactersInString {
+    public static void main(String[] args) {
+        count("abbav");
+    }
+
+    public static Map<Character, Integer> count(String str) {
+        Map<Character, Integer> charactersCount = new LinkedHashMap<>();
+        for (int i = 0; i < str.length(); i++) {
+            charactersCount.putIfAbsent(str.charAt(i), 0);
+            charactersCount.put(str.charAt(i), charactersCount.get(str.charAt(i)) + 1);
+        }
+        return charactersCount;
+    }
 
 }
