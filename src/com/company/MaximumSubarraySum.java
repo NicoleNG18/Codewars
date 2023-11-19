57e5d514abe7a2882217b4fd12f9681fe4fdfe88
Nikol Georgieva <nicole.georgieva18@abv.bg>
2023-11-19 23:26:05 +0200

    Maximum subarray sum solution

--- src/com/company/MaximumSubarraySum.java
+++ src/com/company/MaximumSubarraySum.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.ArrayList;
+
 //The maximum sum subarray problem consists in finding the
 //maximum sum of a contiguous subsequence in an array or list of integers:
 //
@@ -13,4 +15,21 @@ package com.company;
 //Note that the empty list or array is also a valid sublist/subarray.
 
 public class MaximumSubarraySum {
+    public static int sequence(int[] arr) {
+        if (arr.length == 0) {
+            return 0;
+        }
+        int maxSum = 0;
+        int currentSum = 0;
+        for (int j : arr) {
+            currentSum += j;
+            if (currentSum < 0) {
+                currentSum = 0;
+            } else if (currentSum > maxSum) {
+                maxSum = currentSum;
+            }
+        }
+        return maxSum;
+    }
 }
+
